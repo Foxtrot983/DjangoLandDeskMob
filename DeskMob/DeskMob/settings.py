198691ff27f23dd9388ec1b4552d89cae799a40b
Foxtrot983 <wladiksan@gmail.com>
2022-05-30 12:15:25 +0300

    fixes

--- DeskMob/DeskMob/settings.py
+++ DeskMob/DeskMob/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '!7-kl+l46v)0kjkwbnbvw4c9v)^y72v%7672u**6i#^4+z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
